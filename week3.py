bcf877a69bc43caef08660ee300a77a9653c8d14
yoyo1022 <61856767+yoyo1022@users.noreply.github.com>
2020-03-21 17:15:13 +0800

    week3_上課筆記

---
+++ week3.py
@@ -0,0 +1,27 @@
+movie_title = "Avengers: Endgame" 
+release_date = "26 April 2019(USA)" 
+print(movie_title) 
+print(release_date)
+# 以 = 將資訊儲存至一個物件名稱中
+# 以 # 對程式功能做註解 
+# 物件的命名風格:
+# 小寫
+# 中間不能有空格
+# 單字用 _ 分隔
+# 開頭不能使用數字 
+# 不能使用保留字 
+# 不要使用內建函數
+movie_title = "Avengers: Endgame" 
+print(movie_title)
+# 17.18條是錯誤的
+print = "PRINT" 
+print(movie_title)
+# del是刪除變量, 不是刪除數據
+del print 
+print(movie_title)
+# 一開始就要用的內建函數:
+# print() 印出物件所記錄的資料內容 
+# help() 暸解內建函數的作用 
+# type() 暸解物件的類別
+help(print)
+help(help)
